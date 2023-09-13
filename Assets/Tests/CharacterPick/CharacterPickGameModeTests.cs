737230bbaf3467b8f80bb40cdd59bfb687a6f36b
Paata Baliashvili <pl.baliashvili@gmail.com>
2023-09-14 02:11:00 +0300

    Add tests functionality

--- Assets/Tests/CharacterPick/CharacterPickGameModeTests.cs
+++ Assets/Tests/CharacterPick/CharacterPickGameModeTests.cs
@@ -1,15 +1,28 @@
 using System.Collections;
+using PBUnityMultiplayer.Runtime.Core.Client;
+using PBUnityMultiplayer.Runtime.Core.Client.Impl;
+using Tests.Utils;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.TestTools;
+using Utils.ZenjectUtils;
 
 namespace Tests.CharacterPick
 {
-    public class CharacterPickGameModeTests : MonoBehaviour
+    public class CharacterPickGameModeTests : NetworkTestBase
     {
+        protected override void SubstituteResources()
+        {
+            var testNetworkClient = Resources.Load<NetworkClientManager>("NetworkClientManager");
+            
+            ResourceSubstitute.AddSubstitute<INetworkClientManager>(testNetworkClient);
+        }
+        
         [UnityTest]
         public IEnumerator CharacterPickTest()
         {
-            yield return null;
+            SceneManager.LoadScene("Splash");
+            yield return new WaitForSeconds(100f);
         }
     }
 }
---
+++ Assets/Tests/Utils/NetworkTestBase.cs
@@ -0,0 +1,26 @@
+﻿using NUnit.Framework;
+using UnityEngine;
+using Utils.ZenjectUtils;
+using Zenject;
+
+namespace Tests.Utils
+{
+    public abstract class NetworkTestBase
+    {
+        [SetUp]
+        public void Setup()
+        {
+            ResourceSubstitute.Clear();
+            SubstituteResources();
+        }
+
+        protected abstract void SubstituteResources();
+
+        [TearDown]
+        public void TearDown()
+        {
+            ResourceSubstitute.Clear();
+            Object.Destroy(ProjectContext.Instance.gameObject);
+        }
+    }
+}
