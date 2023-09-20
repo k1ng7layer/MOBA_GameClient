39ed8ba873229d7027a8e31221a2f8a7bf891acf
k1ng7layer <75398051+k1ng7layer@users.noreply.github.com>
2023-09-20 20:45:16 +0300

    Add character basic MVP logic

---
+++ Assets/Scripts/Presenters/Impl/CharacterPresenter.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+namespace Presenters.Impl
+{
+    public class CharacterPresenter : ICharacterPresenter
+    {
+        private readonly ICharacterView _characterView;
+        private readonly Models.Character _character;
+
+        public CharacterPresenter(
+            ICharacterView characterView, 
+            Models.Character character
+        )
+        {
+            _characterView = characterView;
+            _character = character;
+        }
+
+        public void SetDestination(Vector3 destination)
+        {
+            _characterView.SetDestination(destination);
+        }
+    }
+}
---
+++ Assets/Scripts/Views/Network/NetworkView.cs
@@ -0,0 +1,31 @@
+using System;
+using PBUnityMultiplayer.Runtime.Core.NetworkObjects;
+using UnityEngine;
+
+namespace Views.Network
+{
+    [RequireComponent(typeof(NetworkObject))]
+    public class NetworkView : MonoBehaviour
+    {
+        private NetworkObject _networkObject;
+        
+        private void Awake()
+        {
+            var hasObject = TryGetComponent<NetworkObject>(out var networkObject);
+
+            if (!hasObject)
+                throw new Exception($"[{nameof(NetworkView)}] view must have NetworkObject component");
+
+            _networkObject = networkObject;
+            
+            OnAwake();
+        }
+
+        protected virtual void OnAwake()
+        { }
+
+        public int OwnerId => _networkObject.OwnerId;
+        public int NetworkObjectId => _networkObject.Id;
+
+    }
+}
